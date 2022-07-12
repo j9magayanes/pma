e1724632f2ff201c9b5b83622761500adc6e5779
jmagayan <janine-aira.magayanes.extern@axelspringer.com>
2022-07-12 19:17:32 +0200

    added post route

--- server/server.js
+++ server/server.js
@@ -2,27 +2,73 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const morgan = require("morgan");
+const sql = require('mssql');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
+
+const sqlConfig = {
+  user: 'pma',
+  password: 'Onedirection@143',
+  database: 'pma',
+  server: 'pricemodeladministration1.database.windows.net',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true// change to true for local dev / self-signed certs
+  }
+}
+app.get("/", async () => {
+  try {
+   // make sure that any items are correctly URL encoded in the connection string
+   await sql.connect(sqlConfig)
+   const result = await sql.query`select * from models`
+   console.log(result)
+  } catch (err) {
+   console.log(err)
+  }
+ }
+)
+ 
 
 // Get all Models
 app.get("/api/v1/models", async (req, res) => {
   try {
     //const results = await db.query("select * from models");
-    const modelRatingsData = await db.query(
-      "select * from models;"
-    );
-
+    await sql.connect(sqlConfig)
+    const modelRatingsData = await sql.query`select * from models`
+    console.log(modelRatingsData)
     res.status(200).json({
       status: "success",
-      results: modelRatingsData.rows.length,
+      results: modelRatingsData.recordset.length,
       data: {
-        models: modelRatingsData.rows,
+        models: modelRatingsData.recordset,
       },
     });
   } catch (err) {
@@ -33,27 +79,25 @@ app.get("/api/v1/models", async (req, res) => {
 //Get a Model
 app.get("/api/v1/models/:id", async (req, res) => {
   console.log(req.params.id);
-
   try {
-    const model= await db.query(
-      "select * from models left join (select model_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by model_id) reviews on models.id = reviews.model_id where id = $1",
-      [req.params.id]
+    await sql.connect(sqlConfig);
+    const model = await sql.query(
+      `select * from models where id = ${req.params.id}`
     );
 
- 
     // select * from models wehre id = req.params.id
 
-    const reviews = await db.query(
-      "select * from reviews where model_id = $1",
+    const reviews = await sql.query(
+      `select * from reviews where model_id = ${req.params.id}`,
       [req.params.id]
     );
     console.log(reviews);
 
     res.status(200).json({
-      status: "succes",
+      status: "success",
       data: {
-        model: model.rows[0],
-        reviews: reviews.rows,
+        model: model.recordset[0],
+        reviews: reviews.recordset,
       },
     });
   } catch (err) {
@@ -61,33 +105,28 @@ app.get("/api/v1/models/:id", async (req, res) => {
   }
 });
 
-// Create a model
+// Create a mode
 
 app.post("/api/v1/models", async (req, res) => {
-  console.log(req.body);
-
   try {
-    const results = await db.query(
-      "INSERT INTO models (name, location, price_range) values ($1, $2, $3) returning *",
-      [req.body.name, req.body.location, req.body.price_range]
-    );
-    console.log(results);
-    res.status(201).json({
-      status: "succes",
-      data: {
-        model: results.rows[0],
-      },
-    });
+   let pool = await sql.connect(sqlConfig)
+   const result = await pool.request()
+   .input('id', sql.Int, req.body.id)
+   .input('name', sql.NVarChar, req.body.name)
+   .input('location', sql.NVarChar, req.body.location)
+   .input('price_range', sql.Int, req.body.price_range)
+   .query('INSERT INTO models (id, name, location, price_range) values (@id, @name, @location, @price_range)')
   } catch (err) {
-    console.log(err);
+   console.log(err)
   }
-});
+ }
+)
 
 // Update Models
 
 app.put("/api/v1/models/:id", async (req, res) => {
   try {
-    const results = await db.query(
+    const results = await sql.query(
       "UPDATE models SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
       [req.body.name, req.body.location, req.body.price_range, req.params.id]
     );
@@ -95,7 +134,7 @@ app.put("/api/v1/models/:id", async (req, res) => {
     res.status(200).json({
       status: "success",
       data: {
-        model: results.rows[0],
+        model: results.recordset[0],
       },
     });
   } catch (err) {
@@ -109,7 +148,7 @@ app.put("/api/v1/models/:id", async (req, res) => {
 
 app.delete("/api/v1/models/:id", async (req, res) => {
   try {
-    const results = db.query("DELETE FROM models where id = $1", [
+    const results = sql.query("DELETE FROM models where id = $1", [
       req.params.id,
     ]);
     res.status(204).json({
@@ -122,7 +161,7 @@ app.delete("/api/v1/models/:id", async (req, res) => {
 
 app.post("/api/v1/models/:id/addReview", async (req, res) => {
   try {
-    const newReview = await db.query(
+    const newReview = await sql.query(
       "INSERT INTO reviews (model_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
       [req.params.id, req.body.name, req.body.review, req.body.rating]
     );
@@ -130,7 +169,7 @@ app.post("/api/v1/models/:id/addReview", async (req, res) => {
     res.status(201).json({
       status: "success",
       data: {
-        review: newReview.rows[0],
+        review: newReview.recordset[0],
       },
     });
   } catch (err) {
@@ -138,16 +177,61 @@ app.post("/api/v1/models/:id/addReview", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`server is up and listening on port ${port}`);
+// Sign up
+app.post("/api/v1/register", (req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+
+  try {
+    const results = bcrypt.hash(password, saltRounds, (err, hash) => {
+      sql.query("INSERT INTO users (name, password) VALUES ($1, $2)", [
+        name,
+        hash,
+      ]);
+      console.log(results);
+      res.status(201).json({
+        status: "success",
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Sign in
+app.post("/api/v1/login", async(req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+
+  try { 
+    const result = await sql.query(
+    "select * from users WHERE name = $1",
+    [name]
+    );
+    if (result.recordset.length > 0) {
+      bcrypt.compare(password, result.recordset[0].password, (error, response) => {
+        if (response) {
+          req.session.user = result;
+          console.log(req.session.user);
+        } else {
+          console.log("wrong pw");
+        }
+      });
+    } else {
+      console.log("doesnt exist");
+    }
+    res.status(201).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err)
+  }
 });
 
 
-// Sign up
-
-
-// Sign in 
-
-
 // Log out
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
